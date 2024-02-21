deb5b467ef181f586f0a471b987d45484216a743
Ines GarciaRuiz <inesgar2004@gmail.com>
2024-02-21 01:37:19 +0100

    Funcionara? v6

--- Vampiro/__init__.py
+++ Vampiro/__init__.py
@@ -63,7 +63,7 @@ def create_app(config_class=Config):
     migrate = Migrate(app, db)
 
     with app.app_context():
-        db.create_all()
+        db.create_all(checkfirst=True)
 
         # Initial setting creation
         if Settings.query.count() == 0:
